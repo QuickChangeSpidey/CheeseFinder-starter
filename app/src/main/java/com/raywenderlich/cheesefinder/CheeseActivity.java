f214a4250c9d749cb74c31b7b19c4a84d2ca4672
akshayLemonaid <akshay@polkadoc.com>
2017-03-22 14:43:05 -0700

    created observable in cheese activity

--- app/src/main/java/com/raywenderlich/cheesefinder/CheeseActivity.java
+++ app/src/main/java/com/raywenderlich/cheesefinder/CheeseActivity.java
@@ -22,6 +22,43 @@
 
 package com.raywenderlich.cheesefinder;
 
+import android.view.View;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.functions.Cancellable;
+
 public class CheeseActivity extends BaseSearchActivity {
+
+    // 1
+    private Observable<String> createButtonClickObservable() {
+
+        // 2
+        return Observable.create(new ObservableOnSubscribe<String>() {
+
+            // 3
+            @Override
+            public void subscribe(final ObservableEmitter<String> emitter) throws Exception {
+                // 4
+                mSearchButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // 5
+                        emitter.onNext(mQueryEditText.getText().toString());
+                    }
+                });
+
+                // 6
+                emitter.setCancellable(new Cancellable() {
+                    @Override
+                    public void cancel() throws Exception {
+                        // 7
+                        mSearchButton.setOnClickListener(null);
+                    }
+                });
+            }
+        });
+    }
 
 }
